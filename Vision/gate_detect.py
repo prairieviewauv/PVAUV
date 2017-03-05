dd9dec78e3963abba59176e285f9c7148ebfcc4b
[mclambchops] <[mclamb.christopher@gmail.com]>
2017-03-04 23:17:51 -0600

    Eyes nearing completion

--- Vision/gate_detect.py
+++ Vision/gate_detect.py
@@ -1,60 +1,29 @@
 import cv2
-import sys
+import rospy
 import time
+import visualizer
 
 
-
-#sets path to gate image cascade
-#cascPath = sys.argv[1]
 
 #initializes farame capture from CAM_0
-cap0 = cv2.VideoCapture(0)
+cap0 = visualizer()
 
-# cap1 = cv2.VideoCapture(1)
 
 while(True):
     # Capture frame-by-frame
-<<<<<<< HEAD
-    ret, frame = cap.read()
-    """
-=======
-    ret0, frame0 = cap0.read()
-    #ret1, frame1 = cap1.read()
->>>>>>> 2a4112adbeeab4e6fb9305909d4fb8be963a57c0
-    # Our operations on the frame come here
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-	# Create the haar cascade
-	#gateCascade = cv2.CascadeClassifier(cascPath)
-
-    # Detect faces in the image
-	# gates = faceCascade.detectMultiScale(
-	#     gray,
-	#     scaleFactor=1.1,
-	#     minNeighbors=5,
-	#     minSize=(30, 30),
-	#     flags = cv2.CASCADE_SCALE_IMAGE
-	#       )
-	# # 
+	ret, frame = cap0.read()
+	cap0.detectGate()
 	# print "Found {0} gates!".format(len(gates))
 
 	# # Draw a rectangle around the gate
 	# for (x, y, w, h) in faces:
 	#     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-"""
-    # Display the resulting frame
-<<<<<<< HEAD
-    cv2.imshow('Frame',frame)
-=======
-    cv2.imshow('Frame',frame0)
->>>>>>> 2a4112adbeeab4e6fb9305909d4fb8be963a57c0
+
+  
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
-cap0.release()
-cv2.destroyAllWindows()
 
 
 
--- Vision/visualizer.py
+++ Vision/visualizer.py
@@ -6,13 +6,12 @@ import time
 
 
 class visualizer:
-    def __init__(self, name, frameRate=24, whRes=640, hRes = 480, camera=0):
-        self.name=name
+    def __init__(self, frameRate=24, whRes=640, hRes = 480, camera=0):
 
         #start video capture stream; return assertion if stream not started
-        self.vidStream= cv2.VideoCapture(camera)
-        if (!vidStream.isOpened()):
-            CV_Assert("I can't see out this eye!")
+        self.vidStream = cv2.VideoCapture(camera)
+        #if (!vidStream.isOpened()):
+         #   CV_Assert("I can't see out this eye!")
 
         #intialize camera capture resolution and frame rate
         vidStream.set(CV_CAP_PROP_FRAME_WIDTH, wRes)
@@ -26,57 +25,115 @@ class visualizer:
         cv2.destroyAllWindows()
 
     #returns frameRate of visualizer object
-    def getFrameRate():
+    def getFrameRate(self):
         return frameRate
 
     #returns tuple containing resolution of visualizer
-    def getRsolution():
+    def getResolution(self):
         return (wRes, hRes)
 
     #captures current frame and writes it to the screen
-    def capFrame():
+    def capFrame(self):
         ret, frame = self.videoStream.read()
         cv2.imshow("Capture", frame)
 
     #returns current frame to process
-    def getFrame():
+    def getFrame(self):
         ret, frame = self.videoStream.read()
         return frame
 
     #converts BGR image to HSV image in order to threshold color in stream
     #detect circle with color
     #return true if circle of color found
-    def detectBuoy(color):
+    def detectBuoy(self, color):
         ret, frame = self.videoStream.read()
         hsv_img = cv2.cvtColor(frame, cvt.COLOR_BGR2HSV)
 
         if color == "red":
-            lower = np.array([0,160,160],dtype="uint8")
-            upper = np.array([10,180,180],dtype="uint8")
+            lower = np.array([0,160,160], dtype="uint8")
+            upper = np.array([10,180,180], dtype="uint8")
         elif color == "green":
-            lower = np.array([45,100,50],dtype="uint8")
-            upper = np.array([75,255,255],dtype="uint8")
+            lower = np.array([45,100,50], dtype="uint8")
+            upper = np.array([75,255,255], dtype="uint8")
         else:
-            print("Please enter a valid buoy color")
-
-        mask = cv2.inRange(hsv_img,lower,upper)
+            print "Please enter a valid buoy color"
+        
+        #thresholding to check if color in image
+        mask = cv2.inRange(hsv_img, lower, upper)
         output = cv2.bitwise_and(hsv_image, hsv_image, mask=mask)
+
         
         #cv2.imshow("{0} detected".format(color), output)
         return True
 
 
-    #detects
-    def detectGate():
-        
-        return
+    #detects 
+    def detectGateLeft(self):
+        ret, frame = self.vidStream.read()
+       
+        frame = cv2.imread(args["image"])
+        height, width, chan = frame.shape
+        left = frame[0:height, 0:width/2]
+        hsv_img = cv2.cvtColor(left, cv2.COLOR_BGR2HSV)
 
-    def detectColor(color):
+        #orange color range
+        lower = np.array([5,50,50], dtype="uint8")
+        upper = np.array([15,255,255], dtype="uint8")
+
+        #thresholding to check if color in image
+        mask = cv2.inRange(hsv_img, lower, upper)
+        output = cv2.bitwise_and(hsv_img, hsv_img, mask=mask)
+
+
+        thresh = cv2.threshold(mask, 5, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh,None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        if len(cnts) > 2:
+            for c in cnts:
+                cv2.drawContours(left, [c], -1, (0,255,0), 2)
+            return "true"
+
+        else:
+            return "false"
+
+
+    def detectGateRight(self):
+        ret, frame = self.vidStream.read()
+       
+        frame = cv2.imread(args["image"])
+        height, width, chan = frame.shape
+        right = frame[0:height, (width/2)+1:width]
+        hsv_img = cv2.cvtColor(right, cv2.COLOR_BGR2HSV)
+
+        #orange color range
+        lower = np.array([5,50,50], dtype="uint8")
+        upper = np.array([15,255,255], dtype="uint8")
+
+        #thresholding to check if color in image
+        mask = cv2.inRange(hsv_img, lower, upper)
+        output = cv2.bitwise_and(hsv_img, hsv_img, mask=mask)
+
+
+        thresh = cv2.threshold(mask, 5, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh,None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        if len(cnts) > 2:
+            for c in cnts:
+                cv2.drawContours(left, [c], -1, (0,255,0), 2)
+            return "true"
+
+        else:
+            return "false"
+
+
+    def detectColor(self, color):
         
         return
     
-    def detectLine(color):
+    def detectLine(self,color):
         return
 
-    def detectChannel():
+    def detectChannel(self):
         return
